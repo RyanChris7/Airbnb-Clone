04315b43dd0f92540180518d0e8e1991325f6c49
RyanChris7 <ryanchrist98@gmail.com>
2023-07-04 12:29:07 +0800

    create map component

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -48,6 +48,7 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -109,7 +110,11 @@ const RentModal = () => {
           title="Where is your place located?"
           subtitle="Help guest find you"
         />
-        <CountrySelect />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map />
       </div>
     );
   }
