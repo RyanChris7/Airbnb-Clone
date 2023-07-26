c1f4cf518cf3384439a87f62333dafd25f8b463a
RyanChris7 <ryanchrist98@gmail.com>
2023-07-26 12:18:38 +0800

    add onOpen for login Modal

--- app/components/modal/RegisterModal.tsx
+++ app/components/modal/RegisterModal.tsx
@@ -41,6 +41,7 @@ const RegisterModal = () => {
       .post("/api/register", data)
       .then(() => {
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error("Something went wrong!");
