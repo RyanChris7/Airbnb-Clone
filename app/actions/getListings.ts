09da453db2d7a9c128abe5801918511c42f49394
RyanChris7 <ryanchrist98@gmail.com>
2023-08-01 11:53:05 +0800

    finish code

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,16 +2,76 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      roomCount,
+      guestCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
 
     let query: any = {};
 
+    //filter logic
     if (userId) {
       query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        some: {
+          OR: [
+            {
+              startDate: { gte: startDate },
+              endDate: { lte: startDate },
+            },
+            {
+              startDate: { lte: endDate },
+              endDate: { gte: endDate },
+            },
+          ],
+        },
+      };
     }
 
     const listings = await prisma.listing.findMany({
